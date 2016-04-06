ece5dd2d17d1377edcdfe3276798b50145a26465
Nan Xiong <nanxiong2014@gmail.com>
2016-04-06 00:34:57 -0700

    test

--- lapack-3.6.0/SRC/checkFT.f
+++ lapack-3.6.0/SRC/checkFT.f
@@ -21,9 +21,9 @@
             d2 = C2-R2
             d1abs = d1
 
-*            IF ( d1 .LT. 0.0D+0)
-*               d1abs = -1 * d1
-*            END IF
+            IF ( d1 .LT. 0.0D+0)
+               d1abs = -1 * d1
+            END IF
 
 *            IF (d1abs.GT. e)
 *          print *,"maybe error"
